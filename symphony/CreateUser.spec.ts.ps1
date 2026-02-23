d380aa8ff6a3dcae8456d2156f965e63ddc7a97c
BrindhaThirumoorthy <bindube2001@gmail.com>
2026-02-23 08:09:49 +0000

    USer Creation Code added

---
+++ symphony/CreateUser.spec.ts.ps1
@@ -0,0 +1,6 @@
+$Env:REPORT_PATH="E:\Playwright\playwright-report\29733d8e-90f9-404f-aabd-a83ffb083121"
+$Env:OUTPUT_PATH="E:\Playwright\playwright-report"
+$Env:SCREENSHOT_PATH="E:\Playwright\screenshotDir"
+$Env:REPORT_FORMATS="html,json"
+Set-Location "E:\Playwright"
+& npx playwright test "CreateUser.spec.ts" --headed --project "Google Chrome" -g "Philips Instance" --workers=1 --repeat-each=1 
