03ded979e30f2ed6126454a908b27af97b4edce6
BrindhaThirumoorthy <bindube2001@gmail.com>
2026-02-23 12:12:05 +0000

    Usercreatin Code changes

--- Variables/data.ts
+++ Variables/data.ts
@@ -2,7 +2,7 @@ export const baseUrl=`$baseUrl`;
 export const userName=`$userName`;
 export const passWord=`$password`;
 export const email = `$email`;
-export const fullName="$fullName"
-export const NewUser="$NewUser"
+export const fullName=`$fullName`;
+export const NewUser=`$NewUser`;
 
 
